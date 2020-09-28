83cc0d4464cad5e94e8dbb2bf263c4489a9c2039
Lars-Petter Helland <lars-petter.helland@hvl.no>
2020-09-28 09:32:49 +0200

    F13 - Skjelett for eksempel lagt ut

---
+++ F13 - Webshop Sesjoner Innlogging/src/no/hvl/dat108/Validator.java
@@ -0,0 +1,13 @@
+package no.hvl.dat108;
+
+public class Validator {
+
+    /**
+     * @param username Brukernavnet som skal valideres
+     * @return Om brukenavnet er gyldig eller ikke
+     * 
+     */
+    public static boolean isValidUsername(String username) {
+    	return false;
+    }
+}
