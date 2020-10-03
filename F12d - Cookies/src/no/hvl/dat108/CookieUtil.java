f55be6f265f7adf3fdae8183a29a30892d41f7e5
Roger Karlsen <mr.haggbart@gmail.com>
2020-10-03 20:04:19 +0200

    NoSuchElementException: No value present fix

--- F12d - Cookies/src/no/hvl/dat108/CookieUtil.java
+++ F12d - Cookies/src/no/hvl/dat108/CookieUtil.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.Arrays;
+import java.util.Optional;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -11,38 +12,35 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CookieUtil {
 
-	private final static String CHARACTER_ENCODING = "UTF-8";
-	private final static int MAX_AGE_IN_SECONDS = 30;
+    private final static String CHARACTER_ENCODING = "UTF-8";
+    private final static int MAX_AGE_IN_SECONDS = 30;
 
-	public static void addCookieToResponse(HttpServletResponse response, String navn, String verdi) {
-		try {
-			Cookie cookie = new Cookie(navn, URLEncoder.encode(verdi, CHARACTER_ENCODING));
-			cookie.setMaxAge(MAX_AGE_IN_SECONDS);
-			response.addCookie(cookie);
-		} catch (UnsupportedEncodingException e) {
-			// Should never happen
-			e.printStackTrace();
-		}
-	}
+    public static void addCookieToResponse(HttpServletResponse response, String navn, String verdi) {
+        try {
+            Cookie cookie = new Cookie(navn, URLEncoder.encode(verdi, CHARACTER_ENCODING));
+            cookie.setMaxAge(MAX_AGE_IN_SECONDS);
+            response.addCookie(cookie);
+        } catch (UnsupportedEncodingException e) {
+            // Should never happen
+            e.printStackTrace();
+        }
+    }
 
-	public static String getCookieFromRequest(HttpServletRequest request, String navn) {
-		String verdi = null;
-		try {
-			verdi = Arrays.stream(request.getCookies())
-					.filter(c -> c.getName().equals(navn))
-					.map(Cookie::getValue)
-					.findFirst().orElseThrow();
+    public static String getCookieFromRequest(HttpServletRequest request, String navn) {
 
-			verdi = URLDecoder.decode(verdi, CHARACTER_ENCODING);
-
-		} catch (NullPointerException e) {
-			// If no cookie in request.
-			// null is returned
-			
-		} catch (UnsupportedEncodingException e) {
-			// Should never happen
-			e.printStackTrace();
-		}
-		return verdi;
-	}
+        if (request.getCookies() == null) {
+            return null;
+        }
+        Optional<String> verdi = Arrays.stream(request.getCookies())
+                .filter(c -> c.getName().equals(navn))
+                .map(Cookie::getValue)
+                .findFirst();
+        try {
+            return verdi.isPresent() ? URLDecoder.decode(verdi.get(), CHARACTER_ENCODING) : null;
+        } catch (UnsupportedEncodingException e) {
+            // Should never happen
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
