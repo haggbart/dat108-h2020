8077818ceebb9471f54b424584d2d1838a0cf857
Roger Karlsen <mr.haggbart@gmail.com>
2020-10-07 22:24:19 +0200

    forenklet metoder ved å bruke standardcharsets

--- F12d - Cookies/src/no/hvl/dat108/CookieUtil.java
+++ F12d - Cookies/src/no/hvl/dat108/CookieUtil.java
@@ -1,10 +1,10 @@
 package no.hvl.dat108;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.Optional;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -12,35 +12,23 @@ import javax.servlet.http.HttpServletResponse;
 
 public class CookieUtil {
 
-    private final static String CHARACTER_ENCODING = "UTF-8";
+    private final static Charset CHARSET = StandardCharsets.UTF_8;
     private final static int MAX_AGE_IN_SECONDS = 30;
 
     public static void addCookieToResponse(HttpServletResponse response, String navn, String verdi) {
-        try {
-            Cookie cookie = new Cookie(navn, URLEncoder.encode(verdi, CHARACTER_ENCODING));
-            cookie.setMaxAge(MAX_AGE_IN_SECONDS);
-            response.addCookie(cookie);
-        } catch (UnsupportedEncodingException e) {
-            // Should never happen
-            e.printStackTrace();
-        }
+        Cookie cookie = new Cookie(navn, URLEncoder.encode(verdi, CHARSET));
+        cookie.setMaxAge(MAX_AGE_IN_SECONDS);
+        response.addCookie(cookie);
     }
 
     public static String getCookieFromRequest(HttpServletRequest request, String navn) {
-
         if (request.getCookies() == null) {
             return null;
         }
-        Optional<String> verdi = Arrays.stream(request.getCookies())
+        return Arrays.stream(request.getCookies())
                 .filter(c -> c.getName().equals(navn))
-                .map(Cookie::getValue)
-                .findFirst();
-        try {
-            return verdi.isPresent() ? URLDecoder.decode(verdi.get(), CHARACTER_ENCODING) : null;
-        } catch (UnsupportedEncodingException e) {
-            // Should never happen
-            e.printStackTrace();
-        }
-        return null;
+                .findAny()
+                .map(c -> URLDecoder.decode(c.getValue(), CHARSET))
+                .orElse(null);
     }
 }
